18d3fa3dbe4d139c09d75034a95773a5f7bcac4d
Pankaj Tejawat <pankaj-edCast@users.noreply.github.com>
2020-10-16 15:57:49 +0530

    added @objc reference to PermissionResult

--- PermissionScope/Structs.swift
+++ PermissionScope/Structs.swift
@@ -72,8 +72,8 @@ import Foundation
 
 /// Result for a permission status request.
 @objc public class PermissionResult: NSObject {
-    public let type: PermissionType
-    public let status: PermissionStatus
+    @objc public let type: PermissionType
+    @objc public let status: PermissionStatus
     
     internal init(type:PermissionType, status:PermissionStatus) {
         self.type   = type
